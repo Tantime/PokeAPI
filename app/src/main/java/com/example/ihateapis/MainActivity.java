f3eca85ea3002953f6c08a6d1d77a859ae653f97
DragonMatt <fluffyphoenix123@gmail.com>
2020-01-28 17:17:15 -0800

    finished the basic functions of the app: - random search - search by id

--- app/src/main/java/com/example/ihateapis/MainActivity.java
+++ app/src/main/java/com/example/ihateapis/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.ihateapis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.squareup.picasso.Picasso;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,8 +19,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonRandom;
+    private Button buttonSearch;
     private TextView textViewName;
+    private EditText editTextSearch;
+
+    private String id = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,37 +54,76 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
-        buttonRandom.setOnClickListener(new View.OnClickListener() {
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 randomPoke(pokeService);
             }
         });
-    }
-
-    public void randomPoke(PokeService pokeService) {
-        Call<Poke> pokeCall = pokeService.getRandomPokemon(Integer.toString((int) (Math.random() * 807 + 1)));
-        pokeCall.enqueue(new Callback<Poke>() {
+        editTextSearch.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onResponse(Call<Poke> call, Response<Poke> response) {
-                // ANY CODE THAT DEPENDS ON THE RESULT OF THE SEARCH HAS TO GO HERE
-                Poke foundPoke = response.body();
-                // check if the body isn't null
-                if (foundPoke != null) {
-                    textViewName.setText(foundPoke.getName());
-                }
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
             }
 
             @Override
-            public void onFailure(Call<Poke> call, Throwable t) {
-                // TOAST THE FAILURE
-                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                id = editTextSearch.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
     }
 
+    public void randomPoke(PokeService pokeService) {
+        if(id == "") {
+            Call<Poke> pokeCall = pokeService.getRandomPokemon(Integer.toString((int) (Math.random() * 807 + 1)));
+            pokeCall.enqueue(new Callback<Poke>() {
+                @Override
+                public void onResponse(Call<Poke> call, Response<Poke> response) {
+                    // ANY CODE THAT DEPENDS ON THE RESULT OF THE SEARCH HAS TO GO HERE
+                    Poke foundPoke = response.body();
+                    // check if the body isn't null
+                    if (foundPoke != null) {
+                        textViewName.setText(foundPoke.getName());
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Poke> call, Throwable t) {
+                    // TOAST THE FAILURE
+                    Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        else {
+            Call<Poke> pokeCall = pokeService.getRandomPokemon(id);
+            pokeCall.enqueue(new Callback<Poke>() {
+                @Override
+                public void onResponse(Call<Poke> call, Response<Poke> response) {
+                    // ANY CODE THAT DEPENDS ON THE RESULT OF THE SEARCH HAS TO GO HERE
+                    Poke foundPoke = response.body();
+                    // check if the body isn't null
+                    if (foundPoke != null) {
+                        textViewName.setText(foundPoke.getName());
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Poke> call, Throwable t) {
+                    // TOAST THE FAILURE
+                    Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
     private void wireWidgets() {
-        buttonRandom = findViewById(R.id.button_main_random);
+        buttonSearch = findViewById(R.id.button_main_search);
         textViewName = findViewById(R.id.textView_main_name);
+        editTextSearch = findViewById(R.id.editText_main_search);
     }
 }
